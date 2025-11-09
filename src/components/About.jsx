92d65fe41772793455d63ab71e3aa5d06c27663c
Alejandro Fernandez <aferesc1409@gmail.com>
2025-11-09 15:11:08 +0100

    Intento subida

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
           <h2>Sobre mí</h2>
           <p>
             Apasionado del mundo de la informática. Me encanta trabajar en equipo y luchar por objetivos en
-            común, dando el máximo de mí al igual que todo el apoyo y ayuda a mis compañeros y mi entorno
+            común dando el máximo de mí, al igual que todo el apoyo y ayuda a mis compañeros y mi entorno
             para realizar dicho trabajo. Me gusta aprender y principalmente de gente con mucha más
             experiencia que yo, para así poder construir mi propio camino. Deseando descubrir nuevas metas y trabajar junto a personas
             que deseen el mismo objetivo.
